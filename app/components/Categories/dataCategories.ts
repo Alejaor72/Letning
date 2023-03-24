5f50350d892c409ff9d93657bf1f1ed7d9dc8bf0
Your Name <you@example.com>
2023-03-23 21:59:29 -0500

    components

---
+++ app/components/Categories/dataCategories.ts
@@ -0,0 +1,45 @@
+interface DataShape {
+    title: string;
+    image: string;
+}
+
+const dataCategories: DataShape[] = [
+    {
+       title: "Garden",
+       image: "/img/garden.png"
+    },
+    {
+        title: "Science",
+        image: "/img/science.png"
+    },
+    {
+        title: "Art",
+        image: "/img/art.png"
+    },
+    {
+        title: "Sports",
+        image: "/img/sports.png"
+    },
+    {
+        title: "Cooking",
+        image: "/img/cooking.png"
+     },
+     {
+        title: "Gaming",
+        image: "/img/gaming.png"
+     },
+     {
+        title: "Technology",
+        image: "/img/technology.png"
+     },
+     {
+        title: "Make-up",
+        image: "/img/makeup.png"
+     },
+     {
+        title: "Health",
+        image: "/img/health.png"
+     }
+];
+
+export default dataCategories;
---
+++ app/components/export.ts
@@ -0,0 +1,2 @@
+export { default as Categories } from "./Categories/Categories.js";
+export { default as Tutorials } from "./Tutorials/Tutorials.js";
